f587faadf911c61f50896c6e5bf7cf9f9e207d11
Mason <masonalorch@gmail.com>
2019-07-29 12:13:19 -0700

    make py folder and add doc string

---
+++ Py_model/bb_1.py
@@ -0,0 +1,4 @@
+""" This is a program that tests how likely a short-term study is to miss the long-term trend.
+
+We are testing for the likelihood of type 2 errors produced by studies of various lengths.
+"""
