d4ec11b479dce99557cbeaa88b64fc9098f779f9
Mason <masonalorch@gmail.com>
2019-08-14 16:43:54 -0400

    add draft of standardize() function

--- Py_model/bb_1.py
+++ Py_model/bb_1.py
@@ -3,3 +3,18 @@
 We are testing for the likelihood of type 2 errors produced by studies of various lengths.
 """
 import pandas as pd
+
+test = pd.read_csv('test', header = None) "!Doesn't see file!"
+
+def standardize(data):
+    pd.data.columns("year", "response")
+    response = pd.data['response']
+    meanResp = pd.response.mean()
+    stdResp = pd.response.std()
+    standResp = ((response - meanResp)/stdResp)
+    standRespList = pd.standResp.tolist()
+    pd.data.insert(2, "stand.response", standRespList, True)
+    return(data)
+
+test1 = standardize(test)
+print(test1)
